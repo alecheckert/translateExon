b78d088b47c049d23d5a0ddd278bbecaf12e631d
Alec Heckert <alecheckert@Alecs-iMac.local>
2016-11-20 13:53:36 -0800

    changed some annoying behavior in segmentTranscripts.py

--- segmentTranscripts.py
+++ segmentTranscripts.py
@@ -3,6 +3,7 @@ segmentTranscripts.py -- split a CSV containing sequences from
 many genes into separate files, one for each transcript
 '''
 import argparse
+import os
 import numpy as np
 import pandas as pd
 
@@ -32,12 +33,14 @@ def segmentTranscripts(filename, transcript_column='transcript_id', sort_by='ran
 		f = splitIndices(f, transcript_column)
 		unique_transcripts = np.unique(f[transcript_column])
 		for transcript in unique_transcripts:
-			print transcript
+			print transcript, out_prefix
 			transcript_df = f[f[transcript_column]==transcript]
 			transcript_df = transcript_df.sort_values(by=sort_by)
 			transcript_df = transcript_df.set_index(sort_by, drop=False)
-			if not out_prefix:
+			if (not out_prefix) or (len(out_prefix)==0):
 				transcript_df.to_csv('%s.csv' % transcript, index=False)
+			elif out_prefix[-1]=='/':
+				transcript_df.to_csv('%s/%s.csv' % (out_prefix, transcript), index=False)
 			else:
 				transcript_df.to_csv('%s_%s.csv' % (out_prefix, transcript), index=False)
 
@@ -66,11 +69,20 @@ def splitIndices(df, column, delimiter=';'):
 
 if __name__=='__main__':
 	parser = argparse.ArgumentParser(description='split a sequence CSV into separate CSVs, one for each transcript')
-	parser.add_argument('-i', '--infile', type=str, required=True, help='Pandas-format CSV containing the sequence information')
+	parser.add_argument('infile', type=str, help='Pandas-format CSV containing the sequence information')
 	parser.add_argument('-c', '--column', type=str, help="name of the column to split the file by. Default: ``transcript_id''", default='transcript_id')
-	parser.add_argument('-p', '--prefix', type=str, help='output filename prefix', default=None)
+	parser.add_argument('-o', '--outdir', type=str, help='directory to output to', default=None)
 	parser.add_argument('-s', '--sort', type=str, help="name of the column to sort the values by. Default is ``rank''", default='rank')
 	args = parser.parse_args()
-	f = segmentTranscripts(args.infile, transcript_column=args.column, sort_by=args.sort, out_prefix=args.prefix)
+
+	if args.outdir:
+		if not os.path.isdir(args.outdir):
+			os.mkdir(args.outdir)
+		out_prefix = '%s/' % args.outdir
+	else:
+		out_prefix = None
+
+	f = segmentTranscripts(args.infile, transcript_column=args.column, sort_by=args.sort, out_prefix=out_prefix)
+
 	print "Finished"
 	
